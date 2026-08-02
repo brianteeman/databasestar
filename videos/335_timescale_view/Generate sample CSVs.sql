cd8b3b87c9662d0d2921f06d5b1a0f4f4d33190c
Ben Brumm <ben@databasestar.com>
2026-08-02 21:46:28 +1000

    Updated sample CSV scripts

--- videos/335_timescale_view/Generate sample CSVs.sql
+++ videos/335_timescale_view/Generate sample CSVs.sql
@@ -8,9 +8,12 @@
 
 \copy (SELECT (now() - interval '181 days' + make_interval(secs => (n::double precision / 125000000) * 31536000)) AS recorded_at, (random() * 100)::int + 1 AS device_id, round((random() * 40 + 10)::numeric, 2) AS temperature, round((random() * 60 + 20)::numeric, 2) AS humidity FROM generate_series(1, 125000000) AS n) TO 'chunk_5.csv' WITH CSV
 
---Done up to here
+
 \copy (SELECT (now() - interval '135 days' + make_interval(secs => (n::double precision / 125000000) * 31536000)) AS recorded_at, (random() * 100)::int + 1 AS device_id, round((random() * 40 + 10)::numeric, 2) AS temperature, round((random() * 60 + 20)::numeric, 2) AS humidity FROM generate_series(1, 125000000) AS n) TO 'chunk_6.csv' WITH CSV
+
 
 \copy (SELECT (now() - interval '89 days' + make_interval(secs => (n::double precision / 125000000) * 31536000)) AS recorded_at, (random() * 100)::int + 1 AS device_id, round((random() * 40 + 10)::numeric, 2) AS temperature, round((random() * 60 + 20)::numeric, 2) AS humidity FROM generate_series(1, 125000000) AS n) TO 'chunk_7.csv' WITH CSV
 
 \copy (SELECT (now() - interval '43 days' + make_interval(secs => (n::double precision / 125000000) * 31536000)) AS recorded_at, (random() * 100)::int + 1 AS device_id, round((random() * 40 + 10)::numeric, 2) AS temperature, round((random() * 60 + 20)::numeric, 2) AS humidity FROM generate_series(1, 125000000) AS n) TO 'chunk_8.csv' WITH CSV
+
+
